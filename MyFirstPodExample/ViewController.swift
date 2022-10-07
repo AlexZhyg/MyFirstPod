108f679c5669575b538d60012f4b0eddb4cc77d7
Alexander <alexzhy@gmail.com>
2022-10-08 00:48:59 +0300

    Added SwiftyLib.podspec

--- MyFirstPodExample/ViewController.swift
+++ MyFirstPodExample/ViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import MyFirstPod
 
 class ViewController: UIViewController {
-
+    
+    var name = MyFirstPod()
+    name.add(a: 2, b: 2)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(name.add(a: 2, b: 2))
+        
     }
 
 
