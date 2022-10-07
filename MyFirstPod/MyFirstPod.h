f7f3a8c0259fd2228be32183615e6cf040b4018b
Alexander <alexzhy@gmail.com>
2022-10-08 00:03:20 +0300

    Initial Commit

---
+++ MyFirstPod/MyFirstPod.h
@@ -0,0 +1,18 @@
+//
+//  MyFirstPod.h
+//  MyFirstPod
+//
+//  Created by Alexander on 07.10.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFirstPod.
+FOUNDATION_EXPORT double MyFirstPodVersionNumber;
+
+//! Project version string for MyFirstPod.
+FOUNDATION_EXPORT const unsigned char MyFirstPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstPod/PublicHeader.h>
+
+
---
+++ MyFirstPodExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstPodExample
+//
+//  Created by Alexander on 07.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
